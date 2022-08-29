d399d6e1eee29fb0522cfa6aa7b9397d6877dc5a
PhilippBach <bach.philipp@outlook.com>
2022-08-29 11:01:06 +0200

    extend description

--- ui.R
+++ ui.R
@@ -49,10 +49,10 @@ shinyUI(dashboardPage(
               h2("Simpson's Paradox: Cholesterol Example"),
               p("This app illustrates the Simpson's Paradox based on the Cholesterol Example in Glymour et al. (2016, Section 1.2). In this example, we want to assess the effect of exercise on cholesterol in various age groups. The scatter plot below suggests that overall more exercise leads to higher cholesterol. However, if we consider each of the age groups separately, the sign of the effect is negative."),
               h2("Does More Exercise Lead to Higher Cholesterol?"),
-              p("We simulate data to illustrate the Simpson's Paradox in the Cholesterol Example. The Simpson's Paradox describes the phenomenon that a certain statistical association like a positive correlation might hold in a population of interest whereas it is reversed in all subpopulations. In the simulated data example below, we find a positive correlation between exercise and cholesterol if we consider the population irrespective of the information on the age group. However, when we perform such a statistical analysis within every age group, the correlation is found to be negative. This sign-flip can be explained by the fact that people in higher age groups are also more likely to do more exercise. Hence, age is a common cause for cholesterol and exercise."),
+              p("We simulate data to illustrate the Simpson's Paradox in the Cholesterol Example. The Simpson's Paradox describes the phenomenon that a certain statistical association like a positive correlation might hold in a population of interest, whereas it is reversed in all subpopulations. If we consider the population irrespective of the information on the age group, we find a positive correlation between exercise and cholesterol. However, when we perform such a statistical analysis within every age group, the correlation is found to be negative. This sign-flip can be explained by the fact that people in higher age groups are also more likely to do more exercise in our data example. Hence, age is a common cause for cholesterol and exercise, which we have to account for in our analysis."),
               h2("Data Example: Scatter Plot, Causal Diagram, Regression"),
-              p(""),
-              p("Click on the checkbox to see how scatter plot, regression line, regression output and DAG change when we condition on the confounder 'age'."),
+              p("The scatter plot below shows the simulated data points. The color of the points refers to the different age groups."),
+              p("Click on the checkbox to see how scatter plot, regression line, regression output and DAG change when we condition on the confounder 'age'. The regression output shows the coefficent estimate on the variable 'Exercise' with the corresponding regression lines being added to the scatter plot. The Directed Acylcal Graph (DAG) below illustrates that age is a common cause for Exercise and Cholesterol."),
               fluidRow(
                 box(
                   fluidRow(
@@ -68,6 +68,7 @@ shinyUI(dashboardPage(
                     ),
                     box(
                       title = "Regression Output", solidHeader = TRUE, status = boxcol_2,
+                      p("Output from a linear regression of the variable 'Cholesterol' on 'Exercise' according to the specified options: Either the regression is estimated using the entire data set or separately in each age group."),
                       gt_output("regression_tbl"),
                       width = 12
                     )),
@@ -76,11 +77,11 @@ shinyUI(dashboardPage(
                   fluidRow(
                     box(
                       title = "Scatter Plot", solidHeader = TRUE, status = boxcol_2,
-                      plotOutput("age_scatter_plot", height = 450),
+                      plotOutput("age_scatter_plot", height = 350),
                       width = 12),
                     box(
                       title = "DAG", solidHeader = TRUE, status = boxcol_2,
-                      plotOutput("age_dag", height = 450),
+                      plotOutput("age_dag", height = 350),
                       width = 12
                     )),
                   width = 8)
