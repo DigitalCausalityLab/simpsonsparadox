ee070953e9cb339adde7341b56e52d8e2dfbe0f8
Gangli Tan <christiana_tan@outlook.com>
2022-08-31 04:42:32 +0200

    fix DAG without conditioning on age

--- R/plot_DAG.R
+++ R/plot_DAG.R
@@ -1,10 +1,10 @@
 # DAG
 dag_unconditional <- confounder_triangle(x = "Exercise", y = "Cholesterol", z = "Avg.Age", x_y_associated = TRUE) %>%
-   suppressWarnings(ggdag_adjust(
+  ggdag_adjust(
     var = NULL,
     text = FALSE,
     use_labels = "label"
-  )) +
+  ) +
   theme_dag(panel.background = element_rect(fill = "#ffffff", colour = "#ffffff"))
 
 dag_conditional <- confounder_triangle(x = "Exercise", y = "Cholesterol", z = "Avg.Age", x_y_associated = TRUE) %>%
