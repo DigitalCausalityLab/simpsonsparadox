db9b79e2d90de3392046d0a380abbd6b8b046348
PhilippBach <bach.philipp@outlook.com>
2023-03-20 15:00:04 +0100

    position of legend inside plot

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -7,7 +7,7 @@ plot_unconditional <-  ggplot(data, aes(y = Cholesterol, x = Exercise
 plot_conditional <- ggplot(data, aes(y = Cholesterol, x = Exercise,
                                      shape = Avg.Age, color = Avg.Age)) +
   geom_point(size = 2) +
-  theme_classic()
+  theme_classic() + theme(legend.position = c(0.95, 0.2))
 
 plot_unconditional_regressionline <- plot_unconditional +
   geom_smooth(formula = y ~ x, method = "lm", se = FALSE, color = "red")
